131d23b4151454c6b0cc3f87ef2911d99dda753f
mmihaylov82 <mmihaylov82@gmail.com>
2017-09-29 00:24:39 +0200

    Added first two homeworks.

---
+++ CSharp Intro and Basic Syntax/Rectangle Area/02.Rectangle Area.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            var area = width * height;
+            Console.WriteLine($"{area:f2}");
+        }
+    }
+}
